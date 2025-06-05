f7308664dad4e6037e7f6e5e91c1a96103d4cace
najmulislamnajim <ninbd17@gmail.com>
2025-06-05 18:54:57 +0600

    add territory home url

--- core/urls.py
+++ core/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from core.views import home, login_view, user_logout
+from core.views import home, login_view, user_logout, territory_home
 
 urlpatterns = [
     path('', home, name='home'),
     path('login/', login_view, name='login'),
     path('logout/', user_logout, name='logout'),
+    path('home/', territory_home, name='territory_home'),
 ]
