16365c32385995bb50d710d7e1499505ccf1a216
najmulislamnajim <ninbd17@gmail.com>
2025-06-14 14:53:12 +0600

    update urls

--- dep_admin/urls.py
+++ dep_admin/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
+from dep_admin import views
 
 urlpatterns = [
-    
+    path('', views.index, name='index'),
+    path('knowledge_series/', views.knowledge_series, name='knowledge_series'),
 ]
