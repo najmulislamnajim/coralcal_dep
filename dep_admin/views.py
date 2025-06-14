a0becc203668bff0babf80474864ff782f7bd3eb
najmulislamnajim <ninbd17@gmail.com>
2025-06-14 14:53:33 +0600

    add knowledge series view

--- dep_admin/views.py
+++ dep_admin/views.py
@@ -1,3 +1,53 @@
-from django.shortcuts import render
+from django.shortcuts import render 
+from django.contrib.auth.decorators import login_required
+from knowledge_series.models import BookWishes
+from django.db.models import Q
+from django.core.paginator import Paginator
 
 # Create your views here.
+@login_required
+def index(request):
+    return render(request, 'index.html')
+
+@login_required
+def knowledge_series(request):
+    if request.method == 'GET':
+        search_query = request.GET.get('search', '')
+        page_number = int(request.GET.get('page') or 1)
+        per_page = int(request.GET.get("per_page") or 10)
+        sort = request.GET.get("sort", "territory")
+        direction = request.GET.get("direction", "asc")        
+        
+        data = BookWishes.objects.select_related('territory').all()
+        if search_query:
+            data = data.filter(
+                Q(dr_id__icontains=search_query) |
+                Q(dr_name__icontains=search_query) |
+                Q(territory__territory__icontains=search_query) |
+                Q(territory__territory_name__icontains=search_query) |
+                Q(territory__region_name__icontains=search_query) |
+                Q(territory__zone_name__icontains=search_query) | 
+                Q(book__icontains=search_query)
+            )
+        sort_by = sort
+        if sort_by == "territory":
+            sort_by = "territory__territory"
+        elif sort_by == "territory_name":
+            sort_by = "territory__territory_name"
+        elif sort_by == "region":
+            sort_by = "territory__region_name"
+        elif sort_by == "zone":
+            sort_by = "territory__zone_name"
+        elif sort_by == "dr_id":
+            sort_by = "dr_id"
+        elif sort_by == "dr_name":
+            sort_by = "dr_name"
+        if direction == "desc":
+            sort_by = f"-{sort_by}"
+        data = data.order_by(sort_by)
+        paginator = Paginator(data, per_page)
+        page_obj = paginator.get_page(page_number)
+    return render(request, 'knowledge_series.html', {
+        'data': page_obj, 'search_query': search_query, 'per_page': per_page, 'sort': sort, 'direction': direction
+    })
+
