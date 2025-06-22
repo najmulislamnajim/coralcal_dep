3ea6eb7f68138edf84cb097f4da5cb98a59f0b5c
najmulislamnajim <ninbd17@gmail.com>
2025-06-22 10:48:58 +0600

    add user_type , create_user

--- core/management/commands/create_user.py
+++ core/management/commands/create_user.py
@@ -1,6 +1,6 @@
 from django.core.management.base import BaseCommand
 from django.contrib.auth.models import User
-from core.models import Territory
+from core.models import Territory, UserProfile
 
 class Command(BaseCommand):
     help = 'Create users for each territory and an admin user'
@@ -8,21 +8,30 @@ class Command(BaseCommand):
     def handle(self, *args, **kwargs):
         # Create users for territories
         territories = Territory.objects.all()
-        default_password = '123456'
+        default_password = 'coralcal'
         for territory in territories:
             username = territory.territory
             if not User.objects.filter(username=username).exists():
-                User.objects.create_user(
+                user =User.objects.create_user(
                     username=username,
                     password=default_password
                 )
+                UserProfile.objects.create(
+                    user=user,
+                    user_type='territory'
+                )
                 self.stdout.write(self.style.SUCCESS(f'Created user for {username}'))
             else:
+                user = User.objects.get(username=username)
+                UserProfile.objects.create(
+                    user=user,
+                    user_type='territory'
+                )
                 self.stdout.write(f'User {username} already exists')
 
         # Create admin user
         admin_username = 'admin'
-        admin_password = 'rpl@123'
+        admin_password = 'coralcal@123'
         if not User.objects.filter(username=admin_username).exists():
             User.objects.create_superuser(
                 username=admin_username,
@@ -31,4 +40,23 @@ class Command(BaseCommand):
             )
             self.stdout.write(self.style.SUCCESS('Created admin user'))
         else:
+            user = User.objects.get(username=admin_username)
+            user.set_password(admin_password)
+            user.save()
             self.stdout.write('Admin user already exists')
+            
+        # Create zone users 
+        for zone in Territory.objects.values_list('zone_name', flat=True).distinct():
+            username = zone.lower().replace(' ', '_')
+            if not User.objects.filter(username=username).exists():
+                user = User.objects.create_user(username=username, password=default_password)
+                UserProfile.objects.create(user=user, user_type='zone', zone_name=zone)
+                self.stdout.write(self.style.SUCCESS(f'Created zone user: {username}'))
+        
+        # Region Users
+        for region in Territory.objects.values_list('region_name', flat=True).distinct():
+            username = region.lower().replace(' ', '_')
+            if not User.objects.filter(username=username).exists():
+                user = User.objects.create_user(username=username, password=default_password)
+                UserProfile.objects.create(user=user, user_type='region', region_name=region)
+                self.stdout.write(self.style.SUCCESS(f'Created region user: {username}'))
--- core/models.py
+++ core/models.py
@@ -1,13 +1,31 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class UserProfile(models.Model):
+    USER_TYPE_CHOICES = (
+        ('territory', 'Territory'),
+        ('admin', 'Admin'),
+        ('super_admin', 'Super Admin'),
+        ('zone', 'Zone'),
+        ('region', 'Region')
+    )
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    user_type = models.CharField(max_length=20, choices=USER_TYPE_CHOICES)
+    
+    zone_name = models.CharField(max_length=100, blank=True, null=True)
+    region_name = models.CharField(max_length=100, blank=True, null=True)
+    
+    def __str__(self):
+        return self.user.username + self.user_type
+    
 class Territory(models.Model):
     """
-    Model to store Terriotory.
+    Model to store Territory.
 
     Attributes:
         territory (CharFiled): Territory code (unique).
-        territory_name (CharField): Name of the terrytory (mandatory).
+        territory_name (CharField): Name of the territory (mandatory).
         region_name (CharField): Name of the region (mandatory).
         zone_name (CharField): Name of the zone (mandatory).
     """
