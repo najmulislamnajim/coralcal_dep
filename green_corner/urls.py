2b3b653e33bfd6022741c2ccc702dd66c4751605
najmulislamnajim <ninbd17@gmail.com>
2025-07-07 14:47:25 +0600

    add green corner form view

--- green_corner/urls.py
+++ green_corner/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
+from . import views
 
 urlpatterns = [
-    
+    path('upload', views.rgc_form, name='rgc_upload'),
 ]
--- green_corner/views.py
+++ green_corner/views.py
@@ -1,3 +1,35 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from green_corner.models import GreenCorner
+from core.models import Territory
 
 # Create your views here.
+def rgc_form(request):
+    if request.method == 'POST':
+        territory_id = request.user
+        dr_id = request.POST.get('dr_id')
+        dr_name = request.POST.get('dr_name')
+        dr_address = request.POST.get('dr_address')
+        rm_phone = request.POST.get('rm_phone')
+        first_flower_plant = request.POST.get('first_flower_plant')
+        second_flower_plant = request.POST.get('second_flower_plant')
+        third_flower_plant = request.POST.get('third_flower_plant')
+        first_medicinal_plant = request.POST.get('first_medicinal_plant')
+        second_medicinal_plant = request.POST.get('second_medicinal_plant')
+        
+        if not dr_id or not dr_name or not dr_address or not rm_phone or not first_flower_plant or not second_flower_plant or not third_flower_plant or not first_medicinal_plant or not second_medicinal_plant:
+            messages.error(request, "Please fill in all the fields.")
+            redirect('rgc_upload')
+        print("Received POST request with data:",territory_id, dr_id, dr_name, dr_address, rm_phone, first_flower_plant, second_flower_plant, third_flower_plant, first_medicinal_plant, second_medicinal_plant)
+        try:
+            territory= Territory.objects.get(territory=territory_id)
+            GreenCorner.objects.create(
+                territory=territory,dr_id=dr_id, dr_name=dr_name, dr_address=dr_address, rm_phone=rm_phone, first_flower_plant=first_flower_plant, second_flower_plant=second_flower_plant, third_flower_plant=third_flower_plant, first_medicinal_plant=first_medicinal_plant, second_medicinal_plant=second_medicinal_plant
+            )
+            messages.success(request, "Green Corner data added successfully.")
+            return redirect('rgc_upload')
+        except Exception as e:
+            messages.error(request, "Error adding Green Corner data: " + str(e))
+            print("Error adding Green Corner data: " + str(e))
+            return redirect('rgc_upload')
+    return render(request, 'rgc_form.html', {"count":1})
