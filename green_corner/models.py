9bb39d654fc99cf61cbe37a221c98c811f1908a3
najmulislamnajim <ninbd17@gmail.com>
2025-07-07 12:10:19 +0600

    add green corner modelL

--- green_corner/models.py
+++ green_corner/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from core.models import Territory
 
 # Create your models here.
+class GreenCorner(models.Model):
+    territory = models.ForeignKey(Territory, on_delete=models.CASCADE)
+    dr_id = models.CharField(max_length=20, unique=True)
+    dr_name = models.CharField(max_length=100, blank=True, null=True)
+    dr_address = models.CharField(max_length=200, blank=True, null=True)
+    rm_phone = models.CharField(max_length=20, blank=True, null=True)
+    first_flower_plant = models.CharField(max_length=30, blank=True, null=True)
+    second_flower_plant = models.CharField(max_length=30, blank=True, null=True)
+    third_flower_plant = models.CharField(max_length=30, blank=True, null=True)
+    first_medicine_plant = models.CharField(max_length=30, blank=True, null=True)
+    second_medicine_plant = models.CharField(max_length=30, blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
