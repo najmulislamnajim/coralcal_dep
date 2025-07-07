108c031752a76b329a954f46a233e71b59bfdc3f
najmulislamnajim <ninbd17@gmail.com>
2025-07-07 17:09:33 +0600

    add green corner view

--- green_corner/views.py
+++ green_corner/views.py
@@ -2,8 +2,10 @@ from django.shortcuts import render, redirect
 from django.contrib import messages
 from green_corner.models import GreenCorner
 from core.models import Territory
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
+@login_required
 def rgc_form(request):
     if request.method == 'POST':
         territory_id = request.user
