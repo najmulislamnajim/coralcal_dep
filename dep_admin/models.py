a49b122784a24d63706538824ba6652ef8e059b0
najmulislamnajim <ninbd17@gmail.com>
2025-07-10 16:20:24 +0600

    add access control model

--- dep_admin/models.py
+++ dep_admin/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+class AccessControl(models.Model):
+    key=models.CharField(max_length=50)
+    is_active=models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    
+    class Meta:
+        db_table = 'access_control'
+        verbose_name = 'Access Control'
+        verbose_name_plural = 'Access Control'
+        
+    def __str__(self):
+        return self.key+"-"+str(self.is_active)
