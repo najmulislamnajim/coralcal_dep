0ea6f53333ec1f30eae7fb6b6d33b11124285f4f
najmulislamnajim <ninbd17@gmail.com>
2025-07-14 17:40:26 +0600

    add doctors data form

--- doctors_data/urls.py
+++ doctors_data/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    
+    path('', views.dd_form, name='dd_form'),
 ]
--- doctors_data/views.py
+++ doctors_data/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
 
 # Create your views here.
+def dd_form(request):
+    return render(request, 'dd_form.html')
