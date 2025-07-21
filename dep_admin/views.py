33bcb13b9976d8e38442029269207e702ba0428d
najmulislamnajim <ninbd17@gmail.com>
2025-07-21 11:16:48 +0600

    add filter based export

--- dep_admin/views.py
+++ dep_admin/views.py
@@ -31,44 +31,8 @@ def knowledge_series(request):
         per_page = int(request.GET.get("per_page") or 10)
         sort = request.GET.get("sort", "territory")
         direction = request.GET.get("direction", "asc")        
-        
-        data = BookWishes.objects.select_related('territory').all()
-        
-        try:
-            profile = request.user.userprofile
-            if profile.user_type == 'zone':
-                data = data.filter(territory__zone_name=profile.zone_name)
-            elif  profile.user_type == 'region':
-                data = data.filter(territory__region_name=profile.region_name)
-        except UserProfile.DoesNotExist:
-            if not request.user.is_superuser:
-                data = BookWishes.objects.none()
-        if search_query:
-            data = data.filter(
-                Q(dr_id__icontains=search_query) |
-                Q(dr_name__icontains=search_query) |
-                Q(territory__territory__icontains=search_query) |
-                Q(territory__territory_name__icontains=search_query) |
-                Q(territory__region_name__icontains=search_query) |
-                Q(territory__zone_name__icontains=search_query) | 
-                Q(book__icontains=search_query)
-            )
-        sort_by = sort
-        if sort_by == "territory":
-            sort_by = "territory__territory"
-        elif sort_by == "territory_name":
-            sort_by = "territory__territory_name"
-        elif sort_by == "region":
-            sort_by = "territory__region_name"
-        elif sort_by == "zone":
-            sort_by = "territory__zone_name"
-        elif sort_by == "dr_id":
-            sort_by = "dr_id"
-        elif sort_by == "dr_name":
-            sort_by = "dr_name"
-        if direction == "desc":
-            sort_by = f"-{sort_by}"
-        data = data.order_by(sort_by)
+        # Get data usnig utils filter function
+        data = utils.filter_knowledge_series_data(request)
         paginator = Paginator(data, per_page)
         page_obj = paginator.get_page(page_number)
     return render(request, 'knowledge_series.html', {
@@ -77,10 +41,6 @@ def knowledge_series(request):
     
 @login_required 
 def export_knowledge_series(request):
-    """
-    Export the knowledge series data to an Excel file.
-    """
-
     # Create a new workbook and add a worksheet
     workbook = openpyxl.Workbook()
     worksheet = workbook.active
@@ -90,18 +50,11 @@ def export_knowledge_series(request):
     headers = ['Dr. RPL ID', 'Dr. Name', 'Territory ID', 'Territory Name', 'Region', 'Zone', 'Book']
     worksheet.append(headers)
     
+    # Get data using the utils function
+    data = utils.filter_knowledge_series_data(request)
+    
     # Populate the worksheet with data
-    queryset = BookWishes.objects.select_related('territory')
-    try:
-        profile = request.user.userprofile
-        if profile.user_type == 'zone':
-            queryset = queryset.filter(territory__zone_name=profile.zone_name)
-        elif  profile.user_type == 'region':
-            queryset = queryset.filter(territory__region_name=profile.region_name)
-    except UserProfile.DoesNotExist:
-        if not request.user.is_superuser:
-            queryset = BookWishes.objects.none()
-    for obj in queryset:
+    for obj in data:
         row = [
             obj.dr_id,
             obj.dr_name,
